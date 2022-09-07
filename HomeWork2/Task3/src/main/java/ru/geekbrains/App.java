e336e48b23694f0b3ee1f6eef4e1bd28dd139e20
Vladimir <wel.tall@yandex.ru>
2022-09-08 00:21:31 +0300

    complited Task 3

---
+++ HomeWork2/Task3/src/main/java/ru/geekbrains/App.java
@@ -0,0 +1,46 @@
+package ru.geekbrains;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class App 
+{
+ public static void main(String[] args){
+    String path="/home/vladimir/Документы/Java/BasicJava/HomeWork2/Task3/src/main/java/ru/geekbrains/data.json";
+    try{
+        JSONArray jsonObject=FileReaderJSON(path);
+        ParseData(jsonObject);
+        }
+    catch(NullPointerException e){
+        System.out.println("Файл не найден");
+        }
+    }
+
+static JSONArray FileReaderJSON(String path){
+        JSONParser parser = new JSONParser();
+        try (Reader reader = new FileReader(path)){
+        JSONArray jsonObject = (JSONArray) parser.parse(reader);
+        return jsonObject;
+    }
+    catch (IOException | ParseException ex) {
+        return null;
+        }
+    }
+static void ParseData(JSONArray jsonObject){
+        StringBuilder builder=new StringBuilder();
+        for (int i = 0; i < jsonObject.size(); i++) {
+            JSONObject str = (JSONObject) jsonObject.get(i);
+            builder.append("Студент ").append(str.get("фамилия")).
+            append(" получил ").append(str.get("оценка")).
+            append(" по предмету ").append(str.get("предмет").toString().toLowerCase());
+            System.out.println(builder);
+            builder.setLength(0);;
+        }
+    }
+}
